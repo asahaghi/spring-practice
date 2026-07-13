3359ebb34abc2cf7b5b33f11eb5828150ee11e8a
sahii <asahaghi19@gmail.com>
2026-07-13 12:16:37 +0330

    Project Started

---
+++ src/main/java/com/ambro/task_manger/TaskMangerApplication.java
@@ -0,0 +1,13 @@
+package com.ambro.task_manger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskMangerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskMangerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ambro/task_manger/TaskMangerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ambro.task_manger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskMangerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
